d5306efd785c07d44a7258128a54e97c77e3f7b3
DeividMesquita <deividsond08@gmail.com>
2024-11-14 14:10:40 -0300

    Feat: subpagina, breadcrumb e accordion

---
+++ assents/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.accordion_box:first').addClass('active')
+    $('.accordion_box:first').children('acc_trigger').children('selector')
+    $('.accordion_box:first').addClass('active')
+})
